961780a4f02d9acc3d748bc1d14362a2f19e8406
ivoal <ivoalsilva@hotmail.com>
2026-06-01 23:51:13 -0300

    estrutura dados das categorias da vitrine

---
+++ src/data/categorias.js
@@ -0,0 +1,42 @@
+export const categorias = [
+    {
+        nome: "Bebidas",
+        imagem: "/imgCategorias/bebidas.webp",
+        slug: "bebidas"
+    },
+    {
+        nome: "Celulares",
+        imagem: "/imgCategorias/celulares.webp",
+        slug: "celulares"
+    },
+    {
+        nome: "Cervejaria",
+        imagem: "/imgCategorias/cervejaria.webp",
+        slug: "cervejaria"
+    },
+    {
+        nome: "Doceria",
+        imagem: "/imgCategorias/doceria.webp",
+        slug: "doceria"
+    },
+    {
+        nome: "Rack para sala",
+        imagem: "/imgCategorias/rack-para-sala.webp",
+        slug: "rack-para-sala"
+    },
+    {
+        nome: "Salgadinhos",
+        imagem: "/imgCategorias/salgadinhos.webp",
+        slug: "salgadinhos"
+    },
+    {
+        nome: "Smart TV",
+        imagem: "/imgCategorias/smartTV.webp",
+        slug: "smarttv"
+    },
+    {
+        nome: "Sofá",
+        imagem: "/imgCategorias/sofa.webp",
+        slug: "sofa"
+    }
+]
