7b6129b070c7d2e3cedf83d9f935dd5b302fd4da
ivoal <ivoalsilva@hotmail.com>
2026-06-08 10:50:37 -0300

    exibe nome e imagem da categoria buscando pelo slug

--- src/app/categoria/[slug]/page.js
+++ src/app/categoria/[slug]/page.js
@@ -1,9 +1,16 @@
 import {categorias} from "@/data/categorias"
+import { notFound } from "next/navigation"
 export default async function CategoriaPage({params}) {
     const {slug} = await params
     const categoria = categorias.find((cat) => cat.slug === slug)
+    if (!categoria) {
+      notFound()
+    }
     return (
-        <h1>{slug}</h1>
+        <main>
+          <h1>{categoria.nome}</h1>
+          <img src={categoria.imagem} alt={categoria.nome} />
+        </main>
     )
 };
 // await = "espera ficar pronto antes de continuar";
