da0bd7bb78d1962d148cad82a80be15ae6dd110c
ivoal <ivoalsilva@hotmail.com>
2026-06-10 21:38:45 -0300

    estrutura de dados dos produtos com ligação por categoriaSlug

---
+++ src/data/produtos.js
@@ -0,0 +1,72 @@
+export const produtos = [
+    {
+        id: 1,
+        nome: "Bebida Láctea Chocolate Nescau 180ml Vitaminada",
+        preco: 2.99 ,
+        imagem: "/imgProdutos/bebidas/nescau.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 2,
+        nome: "Energético Original Monster 473ml Lata com Taurina e Cafeína",
+        preco: 11.99 ,
+        imagem: "/imgProdutos/bebidas/monster.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 3,
+        nome: "Bebida Láctea Cappuccino Chocolate 3 Corações 250ml Power Whey Zero Lactose",
+        preco: 8.99 ,
+        imagem: "/imgProdutos/bebidas/capuccino.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 4,
+        nome: "Refrigerante Cola Pepsi 200ml Garrafa Sem Glúten",
+        preco: 2.49 ,
+        imagem: "/imgProdutos/bebidas/pepsi.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 5,
+        nome: "Refresco de Guaraná Natural Guaravita 290ml Copo Vegano",
+        preco: 2.29 ,
+        imagem: "/imgProdutos/bebidas/refresco-guaravita.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 6,
+        nome: "Água Mineral Natural sem Gás Leven 510ml PET Vegana",
+        preco: 2.49 ,
+        imagem: "/imgProdutos/bebidas/agua-mineral.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 7,
+        nome: "Refrigerante Guaraná Antarctica 2L Pet Original Vegano",
+        preco: 11.99 ,
+        imagem: "/imgProdutos/bebidas/guarana-2litros.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 8,
+        nome: "Suco Tutti-Frutti Turma da Mônica Life Mix 200ml Sem Açúcar Vegano",
+        preco: 4.29 ,
+        imagem: "/imgProdutos/bebidas/suco-caixinha-monica.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 9,
+        nome: "Água de Coco Original Kero Coco 200ml Natural",
+        preco: 3.99 ,
+        imagem: "/imgProdutos/bebidas/agua-de-coco.webp",
+        categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 10,
+        nome: "Suco 100% Laranja Del Valle 1L Integral Vegano",
+        preco: 10.99 ,
+        imagem: "/imgProdutos/bebidas/suco-de-laranja.webp",
+        categoriaSlug: "bebidas"
+    }
+]
