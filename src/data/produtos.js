40f0267da2a35f638f1703e70a264037302f4265
ivoal <ivoalsilva@hotmail.com>
2026-06-15 16:44:15 -0300

    adiciona produtos e imagens da categoria celulares

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -68,5 +68,75 @@ export const produtos = [
         preco: 10.99 ,
         imagem: "/imgProdutos/bebidas/suco-de-laranja.webp",
         categoriaSlug: "bebidas"
+    } ,
+    {
+        id: 11,
+        nome: "Smartphone Samsung Galaxy A07 128GB 4GB Câmera 50MP Tela 6.7 Proteção IP54 - Preto",
+        preco: 666.00 ,
+        imagem: "/imgProdutos/celulares/galaxyA07.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 12,
+        nome: "Smartphone Motorola Moto G15 256GB 12GB RAM Boost Câmera 50MP com AI Tela 6.7 NFC - Grafite",
+        preco: 999.00 ,
+        imagem: "/imgProdutos/celulares/motorolaG15.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 13,
+        nome: "Smartphone Xiaomi Redmi Note 14 8+8GB + 256GB, Preto",
+        preco: 2499.99 ,
+        imagem: "/imgProdutos/celulares/xiamiRedmiNote14.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 14,
+        nome: "Apple iPhone 16 128GB Branco 6,1 48MP iOS 5G",
+        preco: 8578.90 ,
+        imagem: "/imgProdutos/celulares/iphone16.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 15,
+        nome: "Smartphone Huawei Pura 80 Pro 512GB 12GB RAM Branco Câm. Quádrupla + Selfie 13MP",
+        preco: 9898.90 ,
+        imagem: "/imgProdutos/celulares/huawei.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 16,
+        nome: "Samsung Galaxy S25 Ultra 5G 256GB Galaxy AI Titânio Cinza 6,9 12GB RAM Câm. Quádrupla 200+50+10+50MP Bateria 5000mAh Dual Chip",
+        preco: 11999.90 ,
+        imagem: "/imgProdutos/celulares/sansungS25.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 17,
+        nome: "Celular Positivo P41 4G 2.4” Dual SIM Rádio FM - Preto",
+        preco: 199.00 ,
+        imagem: "/imgProdutos/celulares/positivo.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 18,
+        nome: "Celular Flip Vita Desbloqueado P9043 Multilaser",
+        preco: 179.99 ,
+        imagem: "/imgProdutos/celulares/fita.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 19,
+        nome: "Telefone Celular Nokia 110, 1,77, NK006, Preto",
+        preco: 149.00 ,
+        imagem: "/imgProdutos/celulares/nokia110.webp",
+        categoriaSlug: "celulares"
+    } ,
+    {
+        id: 20,
+        nome: "Apple iPhone 17 Pro Max 256GB Laranja-cósmico",
+        preco: 13321.99 ,
+        imagem: "/imgProdutos/celulares/iphone17proMax.webp",
+        categoriaSlug: "celulares"
     }
 ]
