860c8a2e6af930fbc39bb66a55e26d243ffca08e
ivoal <ivoalsilva@hotmail.com>
2026-07-01 22:10:38 -0300

    estiliza página de produto com layout de duas colunas

--- src/app/produto/[id]/page.js
+++ src/app/produto/[id]/page.js
@@ -1,14 +1,23 @@
-import {produtos} from "@/data/produtos"
-import {notFound} from "next/navigation"
+import Image from "next/image"
+import { produtos } from "@/data/produtos"
+import { notFound } from "next/navigation"
 
-export default async function ProdutoPage ({params}) {
-    const {id} = await params
+export default async function ProdutoPage({ params }) {
+    const { id } = await params
     const produto = produtos.find((p) => p.id === Number(id))
-    if(!produto) {notFound()}
+    if (!produto) { notFound() }
+    const precoFormatado = produto.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+
     return (
-        <main>
-            <h1>{produto.nome}</h1>
-            <p>{produto.preco}</p>
+        <main className="flex flex-col md:flex-row gap-8 p-4">
+            <div>
+                <Image src={produto.imagem} alt={produto.nome} width={400} height={400}/>
+            </div>
+            <div className="flex flex-col gap-4">
+                <h1 className="text-2xl font-bold">{produto.nome}</h1>
+                <span className="text-3xl font-bold text-red-600">{precoFormatado}</span>
+                <button className="bg-red-600 text-white font-bold px-8 py-3 rounded-lg hover:bg-red-700 transition-colors w-fit">Comprar</button>
+            </div>
         </main>
     )
 }
