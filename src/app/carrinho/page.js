f0acf220d7399c2295719d5b82d969c68ea3de55
ivoal <ivoalsilva@hotmail.com>
2026-07-06 20:11:14 -0300

    estiliza página do carrinho

--- src/app/carrinho/page.js
+++ src/app/carrinho/page.js
@@ -1,21 +1,23 @@
 "use client"
 
+import Image from "next/image";
 import { useCarrinho } from "@/context/CarrinhoContext"
 
 export default function Carrinho() {
     const { itens , remover } = useCarrinho();
     const total = itens.reduce((soma , n) => soma + n.preco , 0);
     return (
-        <main>
-            <h1>Meu carrinho</h1>
+        <main className="p-4 max-w-3xl mx-auto">
+            <h1 className="text-2xl font-bold mb-4">Meu carrinho</h1>
             {itens.map(p => (
-                <div key={p.id}>
-                    {p.nome}
-                    {p.preco}
-                    <button onClick={() => remover(p.id)}>Remover</button>
+                <div className="flex items-center gap-4 border-b py-3" key={p.id}>
+                    <Image className="" src={p.imagem} width={60} height={60} alt={p.nome} />
+                    <span className="flex-1">{p.nome}</span>
+                    {p.preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+                    <button className="bg-red-600 rounded-2xl p-2 text-white cursor-pointer" onClick={() => remover(p.id)}>Remover</button>
                 </div>
             ))}
-            <p>Total: {total}</p>
+            <p className="text-xl font-bold text-right mt-4">Total: {total.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
         </main>
     )
 }
