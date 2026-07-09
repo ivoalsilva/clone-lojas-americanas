7a46ffd3a95963bbf4c644618249eb009d1f169c
ivoal <ivoalsilva@hotmail.com>
2026-07-09 16:37:52 -0300

    adiciona testes e2e do carrinho

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,21 @@
+describe("carrinho", () => {
+    it("adicionar produto reflete no badge", () => {
+        cy.visit("/categoria/bebidas");
+        cy.get('[aria-label="adicionar ao carrinho"]').first().should("be.visible").click();
+        cy.contains("1").should("be.visible");
+    });
+    it("item aparece na pagina do carrinho", () => {
+        cy.visit("/categoria/bebidas");
+        cy.get('[aria-label="adicionar ao carrinho"]').first().should("be.visible").click();
+        cy.get('a[href="/carrinho"]').click();
+        cy.contains("Nescau").should("be.visible");
+    });
+    it("remover tira o item do carrinho" , () => {
+        cy.visit("/categoria/bebidas");
+        cy.get('[aria-label="adicionar ao carrinho"]').first().should("be.visible").click();
+        cy.get('a[href="/carrinho"]').click();
+        cy.contains("Remover").click();
+        cy.contains("Nescau").should("not.exist");
+        
+    })
+});
